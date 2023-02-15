f7df450ee077cc90102dc07496558443c211bcf3
eldorado7621 <akinolasamuel762@gmail.com>
2023-02-15 02:04:00 +0100

    software validation of the nearest-near IP

---
+++ src/sdCard.h
@@ -0,0 +1,14 @@
+#include <xil_types.h>
+#include "ff.h"
+#include "xil_printf.h"
+#include <xstatus.h>
+#include "xil_cache.h"
+
+int SD_Init();
+int SD_Eject();
+FIL* openFile(char *FileName,char mode);
+u32 closeFile(FIL* fptr);
+int readFile(FIL *fil, u32 DestinationAddress);
+int writeFile(FIL* fptr, u32 size, u32 SourceAddress);
+int nReadFile(char *FileName, u32 DestinationAddress);
+int ReadFile(FIL *fil, u32 DestinationAddress);
