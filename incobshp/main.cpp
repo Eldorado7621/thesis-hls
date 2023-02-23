1c2745ba0eb139e5fe35456cfafed898aa4a727c
eldorado7621 <akinolasamuel762@gmail.com>
2023-02-23 12:35:38 +0100

    half precision implementation extended to calculate minimum cost from neighbouring nodes

--- incobshp/main.cpp
+++ incobshp/main.cpp
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "hls_math.h"
 
 
 #define Node 10000
@@ -65,16 +66,27 @@ bool Colition( Point_2 &A, __fp16 Squared_R_Robot, Segment_3 Edges[MAX_EDGES])
 bool Obstacle_Free( Position_Holonomic &A, Position_Holonomic &B, __fp16 Squared_R_Robot,
 		Segment_3 Edges[MAX_EDGES] )
 {
+
 	bool Bool = true;
 	int N = 5;
 	Position_Holonomic Tem_Positon;
+	float dy,dx;
+
+#pragma HLS BIND_OP variable=dx op=mul impl=fabric latency=4
+#pragma HLS BIND_OP variable=dy op=mul impl=fabric latency=4
+#pragma HLS BIND_OP variable=Tem_Positon op=add impl=fabric latency=2
+
+
+
 	float Delta_x = (A.x - B.x)/(N*1.0);
 	float Delta_y = (A.y - B.y)/(N*1.0);
 
 	for( int i = 0; i <= N; i++)
 	{
-		Tem_Positon.x = Delta_x*i + B.x;
-		Tem_Positon.y = Delta_y*i + B.y;
+		 dx=Delta_x*i ;
+	     dy=Delta_y*i ;
+		Tem_Positon.x = dx+ B.x;
+		Tem_Positon.y = dy+ B.y;
 		Point_2 t( Tem_Positon.x, Tem_Positon.y );
 		if( Colition( t, Squared_R_Robot, Edges ) )
 		{
@@ -86,8 +98,8 @@ bool Obstacle_Free( Position_Holonomic &A, Position_Holonomic &B, __fp16 Squared
 	return Bool;
 }
 
-static void Near(    int X_near[SIZE_NEAR], Node_RRT_Pos RRT_Star_Pos[Node], Node_RRT_Pos &x_new,
-				__fp16 Squared_r_n, short int N , short int &X_near_count)
+static void Near(   short int X_near[SIZE_NEAR], Node_RRT_Pos RRT_Star_Pos[Node], Node_RRT_Pos &x_new,
+		float Squared_r_n, short int N , short int &X_near_count)
 {
 	X_near_count=0;
 	for( int i=0; i<N; i++ )
@@ -99,12 +111,21 @@ static void Near(    int X_near[SIZE_NEAR], Node_RRT_Pos RRT_Star_Pos[Node], Nod
 		}
 	}
 }
+static float Cost( Position_Holonomic &A, Position_Holonomic &B )
+{
+	//Fucion de costo dependiente de la distancia
+	float Dist;
+#pragma HLS BIND_OP variable=Dist op=fadd impl=fabric latency=4
+	 Dist = hls::sqrt(( (A.x - B.x)*(A.x - B.x) ) + ( (A.y - B.y)*(A.y - B.y) ) );
+	return Dist;
+
+}
 
 
 //int nearest(hls::stream<dataFmt> &dataInStream,float x_rand_inpx,float x_rand_inpy,int N)
 //int nearest(hls::stream<axis_data> &dataInStream,Node_Rand x_rand,float x_new_posx,float x_new_posy,int N)
 bool nearest(hls::stream<axis_data> &dataInStream,Node_Rand x_rand,short int N,
-		short int choose,float oup[3],__fp16 Squared_r_n,
+		short int choose,float oup[5],float Squared_r_n,
 		__fp16 Squared_R_Robot,
 		hls::stream<axiso_data> &XNearStream
 )
@@ -120,6 +141,8 @@ bool nearest(hls::stream<axis_data> &dataInStream,Node_Rand x_rand,short int N,
 
 #pragma HLS INTERFACE axis register both port=XNearStream
 #pragma HLS INTERFACE s_axilite port=return bundle=CTRL_BUS
+
+//#pragma HLS INLINE off
 
 	axis_data input_stream;
 	axiso_data output_stream;
@@ -153,6 +176,7 @@ x[1]=2.3;*/
 	}
 
 	int Pos_Nearest_Node=0;
+	int xmin=0;
 	float Dist;
 	Dist= 0;
 	float Tem_Dist;
@@ -164,11 +188,19 @@ x[1]=2.3;*/
 	int rtndval=0;
 
 
-	 int x_near[SIZE_NEAR];
+	short int x_near[SIZE_NEAR];
+	short int List_pos_near[40];
 	float calc_sqr_dst;
-
+	float c_min_local=0;
+	float c_min=0;
+	float cost_tem_local=0;
+	float cost_tem=0;
 	//write the input to the RRT_Star
 	//int i=0;
+
+
+#pragma HLS BIND_OP variable=cost_tem op=fadd impl=fabric latency=2
+
 
 
 
@@ -182,10 +214,6 @@ x[1]=2.3;*/
 		RRT_Star[i].Position.y=node_pt.struct_RRT_Star.Position_y;
 		RRT_Star[i].Location_List=node_pt.struct_RRT_Star.Location_List;
 
-
-		/*}
-	for(int i=0;i<N;i++)
-	{*/
 
 		Tem_Dist = Cal_Squared_Dist( x_rand, RRT_Star[i]);
 		if( w == 0)
@@ -206,24 +234,54 @@ x[1]=2.3;*/
 	}
 
 	x_new_pos = A_StreamReader( RRT_Star[Pos_Nearest_Node], x_rand);
-	oup[0]=x_new_pos.Position.x;
-	oup[1]=x_new_pos.Position.y;
-	oup[2]=(float)RRT_Star[N-1].Location_List;
-	rtndval=Pos_Nearest_Node;
 
 	bool is_obstacle_free=Obstacle_Free( RRT_Star[Pos_Nearest_Node].Position, x_new_pos.Position,
 			Squared_R_Robot, Edges );
-	//oup[0]=(float)is_obstacle_free;
-
 
 	if(is_obstacle_free)
 	{
 
 		Near( x_near, RRT_Star, x_new_pos, Squared_r_n, N,XNearCnt );
+		c_min_local = Cost( RRT_Star[Pos_Nearest_Node ].Position, x_new_pos.Position );
+		c_min = RRT_Star[Pos_Nearest_Node ].Cost + c_min_local;
+
+
+		/*int pos_x_near=0;
+		int List_pos_near_index=0;
+		for(int xnear_index=0;xnear_index<XNearCnt;xnear_index++)
+		{
+			pos_x_near=x_near[xnear_index];
+
+			cost_tem_local = Cost( RRT_Star[pos_x_near ].Position, x_new_pos.Position );
+			cost_tem = RRT_Star[pos_x_near ].Cost + cost_tem_local;
+
+			if( Obstacle_Free( RRT_Star[pos_x_near ].Position, x_new_pos.Position,
+					Squared_R_Robot, Edges))
+			{
+				//List_pos_near.push_back( pos_x_near );
+				List_pos_near[List_pos_near_index]=pos_x_near;
+				List_pos_near_index++;
+
+				if(cost_tem < c_min)
+				{
+					xmin = pos_x_near;
+					c_min = cost_tem;
+					c_min_local = cost_tem_local;
+				}
+			}
+		}*/
+
+		oup[0]=Pos_Nearest_Node;
+		oup[1]=c_min;
+		oup[2]=x_new_pos.Position.x;
+		oup[3]=x_new_pos.Position.y;
+		oup[4]=XNearCnt;
+
+
 
 		for(int j=0;j<XNearCnt;j++)
 		{
-			output_stream.data=(int)RRT_Star[j].Position.x;
+			output_stream.data=x_near[j];
 
 			output_stream.keep=input_stream.keep;
 
@@ -272,58 +330,6 @@ x[1]=2.3;*/
 	}
 
 	return is_obstacle_free;
-
-
-
-
-
-	/* if(choose==2)
-	{
-		for(int i=0;i<N;i++)
-		{
-#pragma HLS PIPELINE
-
-			input_stream=dataInStream.read();
-			node_pt.reg=input_stream.data;
-
-			RRT_Star[i].Position.x=node_pt.struct_RRT_Star.Position_x;
-			RRT_Star[i].Position.y=node_pt.struct_RRT_Star.Position_y;
-			RRT_Star[ i ].Location_List=node_pt.struct_RRT_Star.Location_List;
-
-			calc_sqr_dst=Cal_Squared_Dist( x_rand, RRT_Star[i] );
-			if(  calc_sqr_dst< Squared_r_n )
-			{
-				x_near[XNearCnt]= RRT_Star[i ].Location_List;
-				XNearCnt++;
-			}
-		}
-		rtndval=XNearCnt;
-		for(int j=0;j<XNearCnt;j++)
-		{
-			output_stream.data=x_near[j];
-
-			output_stream.keep=input_stream.keep;
-
-			output_stream.strb=input_stream.strb;
-
-			output_stream.user=input_stream.user;
-
-			output_stream.id=input_stream.id;
-
-			output_stream.dest=input_stream.dest;
-
-			if(j==XNearCnt-1)
-				output_stream.last=1;
-			else
-				output_stream.last=0;
-			XNearStream.write(output_stream);
-
-
-		}
-	}*/
-
-	//return rtndval;
-
 
 
 }
